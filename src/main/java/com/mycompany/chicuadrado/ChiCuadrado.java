94e9fcf4ae74b881433a87ea9ee9f9b5a6156d89
HP <HP@FRANKLIN>
2024-04-27 21:26:18 -0500

    coloque mensaje nuevo

--- src/main/java/com/mycompany/chicuadrado/ChiCuadrado.java
+++ src/main/java/com/mycompany/chicuadrado/ChiCuadrado.java
@@ -11,6 +11,7 @@ package com.mycompany.chicuadrado;
 public class ChiCuadrado {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!");
+         System.out.println("hjcvasjc!");
     }
 }
