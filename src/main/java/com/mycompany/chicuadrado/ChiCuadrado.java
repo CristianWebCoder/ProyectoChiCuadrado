934196638e4d0b4f6b390017d09dc3eb71e0a735
CristianWebCoder <cristianbernalweb@gmail.com>
2024-04-27 21:01:30 -0500

    Carga inicial

---
+++ src/main/java/com/mycompany/chicuadrado/ChiCuadrado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chicuadrado;
+
+/**
+ *
+ * @author Cristian PC
+ */
+public class ChiCuadrado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
